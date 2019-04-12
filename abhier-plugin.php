744aed114763f98cd46ddb4d2488562426e40bb8
Ben Companjen <ben@companjen.name>
2019-04-12 12:34:41 +0200

    Remove ORCID login text
    The code that sets the text of the login button is now in a separate
    plugin.

--- abhier-plugin.php
+++ abhier-plugin.php
@@ -3,22 +3,13 @@
 Plugin Name: Abnormal Hieratic functions
 Plugin URI: https://github.com/LeidenUniversityLibrary/abhier-plugin
 Description: Provide functionality for the Abnormal Hieratic Global Portal
-Version: 0.5.4
+Version: 0.6.0
 Author: bencomp
 Author URI: https://ben.companjen.name
 License: GPLv2 Copyright (c) 2019 Leiden University Libraries
 */
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-
-/**
- * Register login button text update
- */
-add_filter('openid-connect-generic-login-button-text', function( $text ) {
-    $text = __('Login with ORCID');
-    
-    return $text;
-});
 
 /**
  * Register papyrus post type
